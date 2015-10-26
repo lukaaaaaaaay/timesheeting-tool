bbb91ac35a4c3b23e49199da040322817ab51305
Luke Phelan <laphelan@gmail.com>
2015-10-26 23:13:19 +1100

    basic single project view

--- app/app/project/view-project.controller.js
+++ app/app/project/view-project.controller.js
@@ -1,31 +1,42 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular.module('tsm').controller('ViewProjectCtrl', function ($scope, $location, $rootScope, tstBodyClass, Auth, Project, Status, $stateParams, notifier) {
-    var userId = Auth.getCurrentUser().id;
-    $scope.project = {};
-    // set body class
-    $rootScope.bodyClass = tstBodyClass.returned.dashboard;
+    angular.module('tsm').controller('ViewProjectCtrl', function ($scope, $location, $rootScope, tstBodyClass, Auth, Project, Status, $stateParams, notifier) {
+        var userId = Auth.getCurrentUser().id;
+        $scope.project = {};
+        $scope.statuses = [];
+        // set body class
+        $rootScope.bodyClass = tstBodyClass.returned.dashboard;
 
-    function init() {
-        // set sidebar menu
-        $rootScope.sidebarMenu.selected = 3;
-        $rootScope.sidebarMenu.showDropdown = true;
-        $rootScope.sidebarMenu.activeSubmenu = 2;
+        function init() {
+            // set sidebar menu
+            $rootScope.sidebarMenu.selected = 3;
+            $rootScope.sidebarMenu.showDropdown = true;
+            $rootScope.sidebarMenu.activeSubmenu = 2;
 
-        // get active company
-        // ActiveCompany.get({directorId: userId}, function (returnedCompany) {
-        //     var company = returnedCompany;
-        // }, function (error) {
-        //     notifier.error('Error', 'Unable to find company for logged in user. Try logging in again!');
-        // });
+            Project.get({id: $stateParams.id}, function (project) {
+                $scope.project = project;
+            }, function (error) {
+                console.log(error);
+                notifier.error('Error!', error.data);
+            });
 
-        Project.get({id: $stateParams.id}, function (project) {
-            $scope.project = project;
-        }, function (error) {
-            console.log(error);
-            notifier.error('Error!', error.data);
-        });
-    }    
+            Status.query({}, function (statuses) {
+                $scope.statuses = statuses;
+            }, function (error) {
+                console.log(error);
+                notifier.error('Error', 'Unable to retrieve system statuses');
+            });
+        }    
 
-    init();
-});
+        init();
+
+        $scope.statusAsText = function (statusId) {
+            var status = _.find($scope.statuses, {id: statusId})
+            if(status)
+                return status.name;
+
+            return '';
+        };
+    });
+})();
