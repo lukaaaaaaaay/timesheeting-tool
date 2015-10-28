b4248b922e5606e40daeb6bb1e6999f37b29bee5
Chris Clayton <chris@chris-clayton.com>
2015-10-28 23:08:25 +1100

    add modules to loader

--- app/js/app.js
+++ app/js/app.js
@@ -4,5 +4,7 @@
     angular.module(tst.modules.app.name, [
         'ngRoute',
         tst.modules.core.name,
+        tst.modules.auth.name,
+        tst.modules.dashboard.name,
     ]);
 }(angular, tst));
