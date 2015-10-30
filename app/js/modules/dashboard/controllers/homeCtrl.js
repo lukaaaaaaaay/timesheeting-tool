e85c3f91d9b4148ddcd4cb13d163531df2b62c07
Chris Clayton <chris@chris-clayton.com>
2015-10-30 17:41:24 +1100

    :)

--- app/js/modules/dashboard/controllers/homeCtrl.js
+++ app/js/modules/dashboard/controllers/homeCtrl.js
@@ -3,7 +3,20 @@
 
     angular.module(tst.modules.dashboard.name)
         .controller(tst.modules.dashboard.controllers.home, [
-        function () {
+        '$rootScope',
+        function ($rootScope) {
+
+            // todo: is this the best place for this????
+                // Should we abstract this and run on a 'controllerActive' event?
+            // sidebar menu - set in menu tree controllers. 
+            $rootScope.selectedMenu = 1;
+            $rootScope.showDropdown = false;  
+            $rootScope.sidebarMenu = {
+                selected: 1,
+                showDropdown: false,
+                activeSubmenu: 0,
+                collapsed: false
+            };
             
         }
     ]);
