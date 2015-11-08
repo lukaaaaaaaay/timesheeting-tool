1280ef8bc52eb250120f21500536167b09d89194
Chris Clayton <chris@chris-clayton.com>
2015-11-09 09:31:00 +1100

    update auth routes to states

--- app/js/modules/auth/module.auth.routes.js
+++ app/js/modules/auth/module.auth.routes.js
@@ -5,20 +5,27 @@
      * Defines routes handled by the auth module
      */
     angular.module(tst.modules.auth.name).config([
-        '$routeProvider',
-        function ($routeProvider) {
-            $routeProvider.when(tst.modules.auth.routes.login, {
+        '$stateProvider',
+        function ($stateProvider) {
+            $stateProvider.state(tst.modules.auth.states.login, {
+                url: tst.modules.auth.routes.login,
                 controller: tst.modules.auth.controllers.login,
                 templateUrl: tst.modules.auth.views.login,
-                bodyClass: tst.modules.auth.bodyClass.login
-            });
-            $routeProvider.when(tst.modules.auth.routes.notAuthorised, {
-                controller: tst.modules.auth.controllers.login,
-                templateUrl: tst.modules.auth.views.notAuthorised,
-                bodyClass: tst.modules.auth.bodyClass.notAuthorised
+                data: {
+                    bodyClass: tst.modules.auth.bodyClass.login
+                }
             });
 
-            $routeProvider.otherwise({ redirectTo: tst.modules.auth.routes.login });
+            $stateProvider.state(tst.modules.auth.states.notAuthorised, {
+                url: tst.modules.auth.routes.notAuthorised,
+                controller: tst.modules.auth.controllers.login,
+                templateUrl: tst.modules.auth.views.notAuthorised,
+                data: {
+                    bodyClass: tst.modules.auth.bodyClass.notAuthorised
+                }
+            });
+
+            // $routeProvider.otherwise({ redirectTo: tst.modules.auth.routes.login });
         }]);
 
 
