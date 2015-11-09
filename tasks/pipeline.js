8ad34bd1852678345a00dc537cd54c8e544be46c
Luke Phelan <laphelan@gmail.com>
2015-11-09 22:15:08 +1100

    quick updates

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -31,6 +31,7 @@ var jsFilesToInject = [
 
   // Bower Components are brought in here
   '/bower_components/angular/angular.js',
+  '/bower_components/jquery/dist/jquery.js',
   '/bower_components/angular-base64/angular-base64.js',
   '/bower_components/angular-bootstrap/ui-bootstrap.min.js',
   '/bower_components/angular-local-storage/dist/angular-local-storage.js',
