2340488725866f6f86b1bcc9d539cd8c20c88154
Chris Clayton <chris@chris-clayton.com>
2015-11-15 23:57:22 +1100

    add currentCompany to rootScope

---
+++ app/js/modules/company/modules.company.run.js
@@ -0,0 +1,23 @@
+(function (angular, tst) {
+    'use strict';
+
+/*
+ * Kick Starts the auth module
+ */
+angular.module(tst.modules.auth.name).run([
+    '$rootScope',
+    '$location',
+    tst.modules.core.services.eventbus,
+    function ($rootScope, $location, eventbus) {
+
+
+        // Listen for companyRegistered event and add the currentCompany to the $rootScope.
+        eventbus.subscribe(tst.modules.company.events.companyRegistered, function (event, org) {
+            // It's very handy to add references to currentUser to the $rootScope
+            // so that we can access them from any scope within our application.
+            console.log("adding " + org.companyName + " to rootScope");
+            $rootScope.currentCompany = org;
+        });
+
+    }]);
+}(angular, tst));
--- app/js/modules/company/services/api.js
+++ app/js/modules/company/services/api.js
@@ -32,7 +32,7 @@
 
 
                     // Broadcasts a companyRegistered event for subscribers.
-                    // eventbus.broadcast(tst.modules.company.events.companyRegistered, currentCompany);
+                    eventbus.broadcast(tst.modules.company.events.companyRegistered, currentCompany);
                     callback(company);
                 })
                 .error(function(err) {
