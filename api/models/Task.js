3c93ef2239d231ab16ec0067c0795215489e32b8
Craig McQuinn <craigmcquinn@windowslive.com>
2015-11-23 21:51:21 +0000

    Fixed merge conflicts?

--- api/models/Task.js
+++ api/models/Task.js
@@ -5,108 +5,114 @@
  */
 
  var Task = {
- 	tableName: 'tasks',
+    tableName: 'tasks',
 
- 	attributes: {
+    attributes: {
 
- 		// A task has a name
- 		name: {
- 			type: 'string',
- 			required: true
- 		},
+        // A task has a name
+        name: {
+            type: 'string',
+            required: true
+        },
 
- 		// A task has an (optional) description
- 		description: {
- 			type: 'string'
- 		},
+        // A task has an (optional) description
+        description: {
+            type: 'string'
+        },
 
- 		// A task has an (optional) start date
- 		startDate: {
- 			type: 'datetime'
- 		},
+        // A task has an (optional) start date
+        startDate: {
+            type: 'datetime'
+        },
 
- 		// A task has an (optional) due date
- 		dueDate: {
- 			type: 'datetime'
- 		},
+        // A task has an (optional) due date
+        dueDate: {
+            type: 'datetime'
+        },
 
- 		// A task must have a status
- 		statusId: {
- 			model: 'Status',
- 			required: true
- 		},
+        // A task must have a status
+        statusId: {
+            model: 'Status',
+            required: true
+        },
 
- 		// A task is created by a user (TODO)
- 		createdBy: {
- 			model: 'User',
- 		},
+        // A task is created by a user (TODO)
+        createdBy: {
+            model: 'User',
+        },
 
- 		// A task must belong to a project
- 		projectId: {
- 			model: 'Project',
- 			required: true
- 		},
+        // A task must belong to a project
+        projectId: {
+            model: 'Project',
+            required: true
+        },
 
- 		// A task can have many categories
- 		// relationship between tasks and categories stored in separate table
- 		categories: {collection: 'category', through: 'taskcategory'},
+        // A task can have many categories
+        // relationship between tasks and categories stored in separate table
+        // how to map?
+        categories: {collection: 'Category', via: 'tasks'},
 
- 		// A task can have many members
- 		users: {collection: 'user', through: 'taskmember'},
+        // A task can have many members
+        members: {collection: 'User', via: 'tasks'},
 
- 		// A task can have many items
- 		//items: {collection: 'Item', via: 'task'},
+        // A task can have many items
+        //items: {collection: 'Item', via: 'task'},
 
- 		// A task can have many comments
- 		//comments: {collection: 'Comment', via: 'task'},
+        // A task can have many comments
+        //comments: {collection: 'Comment', via: 'task'},
 
- 		// A task can have many timesheets?
- 		//timesheets: {collection: 'Timesheet', via: 'task'},
+        // A task can have many timesheets?
+        //timesheets: {collection: 'Timesheet', via: 'task'},
 
- 		/**
-		 * Finds all tasks for a project.
-		 * 
-		 * @param {int}	projectId - The project id
- 		 */
- 		findAllForProject: function(projectId, cb) {
+        toJSON: function () {
+           var task = this.toObject();
+           return task;
+        }
+    },
 
- 			Task.find({projectId: projectId }).exec(function (err, tasks) {
- 				if (err) return cb(err);
+    /**
+     * Finds all tasks for a project.
+     * 
+     * @param {int} projectId - The project id
+     */
+    findAllForProject: function(projectId, cb) {
 
- 				return cb(null, tasks);
- 			});
- 		},
+        this.find({projectId: projectId }).exec(function (err, tasks) {
+            if (err) return cb(err);
 
- 		/**
- 		 * Finds all tasks by same creator.
- 		 *
- 		 * @param {int}	createdBy - The user id of the creator
- 		 */
+            return cb(null, tasks);
+        });
+    },
 
- 		findAllForCreator: function(createdBy, cb) {
+    /**
+     * Finds all tasks by same creator.
+     *
+     * @param {int} createdBy - The user id of the creator
+     */
 
- 			Task.find({createdBy: createdBy }).exec(function (err, tasks) {
- 				if (err) return cb(err);
+    findAllForCreator: function(createdBy, cb) {
 
- 				return cb(null, tasks);
- 			});
- 		},
+        this.find({createdBy: createdBy }).exec(function (err, tasks) {
+            if (err) return cb(err);
 
- 		/**
- 		 * Finds all tasks based on their status.
- 		 *
- 		 * @param {int} 	statusId - The status id of the task
- 		 */
+            return cb(null, tasks);
+        });
+    },
 
- 		findAllForStatus: function(statusId, cb) {
+    /**
+     * Finds all tasks based on their status.
+     *
+     * @param {int}     statusId - The status id of the task
+     */
 
- 			Task.find({statusId: statusId }).exec(function (err, tasks) {
- 				if (err) return cb(err);
+    findAllForStatus: function(statusId, cb) {
 
- 				return cb(null, tasks);
- 			});
- 		},
- 	},
+        this.find({statusId: statusId }).exec(function (err, tasks) {
+            if (err) return cb(err);
+
+            return cb(null, tasks);
+        });
+    },
  }
 
  module.exports = Task;
