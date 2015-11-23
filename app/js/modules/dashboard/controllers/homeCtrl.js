e5289d2e4a7d6dab7c9354376130b7bf40011dc4
Luke Phelan <laphelan@gmail.com>
2015-11-23 21:17:38 +1100

    finished dashboard

--- app/js/modules/dashboard/controllers/homeCtrl.js
+++ app/js/modules/dashboard/controllers/homeCtrl.js
@@ -6,12 +6,18 @@
         '$scope',
         tst.modules.core.services.notifier,
         tst.modules.auth.services.authentication,
-        tst.modules.project.services.api,   
-        function ($scope, notifier, authentication, projectApi) {
+        tst.modules.project.services.api,
+        //tst.modules.tasks.services.api,   
+        //function ($scope, notifier, authentication, projectApi, taskApi) {
+        function ($scope, notifier, authentication, projectApi, taskApi) {
             $scope.projects = [];
+            $scope.tasks = [];
             var statuses = [];
             $scope.projectBreakdown = [];
             $scope.projectLabels = [];
+            $scope.taskBreakdown = [];
+            $scope.taskLabels = [];
+            var user = authentication.getCurrentLoginUser();
 
             $scope.statusAsText = function (statusId) {
                 var status = _.find(statuses, {id: statusId})
@@ -26,28 +32,66 @@
             };
 
             function init() {
-                var user = authentication.getCurrentLoginUser();
                 
+                projectApi.getAllStatuses(function(allStatuses) {
+                    statuses = allStatuses;
+                    retrieveProjects(allStatuses);
+                    //retrieveTasks(allStatuses)
+                    }, function(error) {
+                        console.log(error);
+                        notifier.error('Error', 'There was an error retrieving all the statuses');
+                    });
+                
+            }
+
+            function retrieveProjects(statuses) {
                 if(user.roleId == 1) {
                     projectApi.getAllProjects(function (projects) {
                         $scope.projects = projects.slice(0,3);
-                        retrieveStatusesAndBreakdown($scope.projects);                    
+                        calculateProjectBreakdown(projects, statuses);                    
                     }, function (error) {
                         notifier.error("Error", "Unable to find projects for the current user");
                     });
+
                 } else if(user.roleId ==2) {
                     projectApi.getAllProjectsForCompany(user.companyId, function (projects) {
                         $scope.projects = projects.slice(0,3);
-                        retrieveStatusesAndBreakdown($scope.projects);
+                        calculateProjectBreakdown(projects, statuses);
                     }, function (error) {
                         notifier.error("Error", "Unable to find projects for the current user");
                     });
                 } else {
                     projectApi.getAllProjectsForUser(user.id, function (projects) {
                         $scope.projects = projects.slice(0,3);
-                        retrieveStatusesAndBreakdown($scope.projects);        
+                        calculateProjectBreakdown(projects, statuses);        
                     }, function (error) {
                         notifier.error("Error", "Unable to find projects for the current user");
+                    });
+                }
+                
+            }
+
+            function retrieveTasks(statuses) {
+                if(user.roleId == 1) {
+                    taskApi.getAllTasks(function (tasks) {
+                        
+                        calculateTaskBreakdown(tasks, statuses);                    
+                    }, function (error) {
+                        notifier.error("Error", "Unable to find tasks for the current user");
+                    });
+
+                } else if(user.roleId ==2) {
+                    taskApi.getAllTasksForCompany(user.companyId, function (tasks) {
+                    
+                        calculateTaskBreakdown(tasks, statuses);
+                    }, function (error) {
+                        notifier.error("Error", "Unable to find tasks for the current user");
+                    });
+                } else {
+                    taskApi.getAllForUser(user.id, function (tasks) {
+                        calculateTaskBreakdown(tasks, statuses);        
+                    }, function (error) {
+                        notifier.error("Error", "Unable to find tasks for the current user");
                     });
                 }
             }
@@ -64,14 +108,16 @@
                 });
             }
 
-            function retrieveStatusesAndBreakdown(projects) {
-                projectApi.getAllStatuses(function(allStatuses) {
-                    statuses = allStatuses;
-                    calculateProjectBreakdown(projects, statuses);
-                    }, function(error) {
-                        console.log(error);
-                        notifier.error('Error', 'There was an error retrieving all the statuses');
-                    });
+            function calculateTaskBreakdown(tasks, statuses) {
+               angular.forEach(statuses, function(status, idx) {
+                    $scope.taskLabels.push(status.name);
+                    var tasksForStatus = _.where(tasks, {statusId: status.id});
+                    if(tasksForStatus)
+                        $scope.taskBreakdown.push(tasksForStatus.length);
+                    else {
+                        $scope.taskBreakdown.push(0);
+                    }
+                }); 
             }
 
             init();
